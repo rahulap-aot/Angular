11e4c964fb94d981a159fc31c3f404e1318a5499
rahulap-aot <rahul.ap@aot-technologies.com>
2024-11-22 03:45:05 -0800

    first commit

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  // string , number , boolean, date, object , array , null , undefined
+  firstName: string = 'John';
+  age: number = 30;
+  isAdmin: boolean = true;
+  birthDate: Date = new Date('1989-05-20');
+
+  inputType: string ="radio";
+  selectedState: string = '';
+}
