e8f4439507e1d781444be1c7624e9563a44c5634
rahulap-aot <rahul.ap@aot-technologies.com>
2024-11-22 22:25:09 -0800

    api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MasterComponent } from './components/master/master.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RolesComponent,MasterComponent],
+  imports: [RouterOutlet, MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,26 +1,67 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  // string , number , boolean, date, object , array , null , undefined
-  firstName: string = 'John';
-  age: number = 30;
-  isAdmin: boolean = true;
-  birthDate: Date = new Date('1989-05-20');
-  inputType: string ="radio";
-  selectedState: string = '';
+export class RolesComponent implements OnInit {
 
-  showWelcome (){
-    alert('Welcome');
-  }
-  showMessage ( messege: string){
-    alert(messege);
-  }
+  roleList: IRole[] = [];
+
+  http = inject(HttpClient)
+
+  // constructor(private http: HttpClient) {
+    
+  // } 
+
+
+ngOnInit(): void {
+  this.getAllRoles();
+}
+
+getAllRoles(){
+  this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles').subscribe((res:any) => {
+    this.roleList = res.data;
+    console.log(this.roleList);
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // // string , number , boolean, date, object , array , null , undefined
+  // firstName: string = 'John';
+  // age: number = 30;
+  // isAdmin: boolean = true;
+  // birthDate: Date = new Date('1989-05-20');
+  // inputType: string ="radio";
+  // selectedState: string = '';
+
+  // showWelcome (){
+  //   alert('Welcome');
+  // }
+  // showMessage ( messege: string){
+  //   alert(messege);
+  // }
 
 }
